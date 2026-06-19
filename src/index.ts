1b592b948d7b50b131b5615b8cb56b6d0fed9858
Wesley Damasceno <wesleydmscno@gmail.com>
2026-06-19 01:26:55 -0300

    refactor: improve exported zod types and integrate locale system to z object

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import * as zod from "zod";
+
+import { zodLocale, type _ZodLocaleState } from "./config/locale";
 import * as validators from "./validators";
 import * as coerces from "./coerces";
 
 type InferredZodTypes = typeof zod;
-type ValidatorsTypes = typeof validators;
-type CoercesTypes = typeof coerces;
-type ZCNTypes = InferredZodTypes & ValidatorsTypes & CoercesTypes;
+type _ValidatorsTypes = typeof validators;
+type _CoercesTypes = typeof coerces;
 
-const z = Object.assign({}, zod, validators, coerces) as ZCNTypes;
+type ZKit = InferredZodTypes &
+  _ValidatorsTypes &
+  _CoercesTypes & { locale: _ZodLocaleState };
+
+const z = Object.assign({}, zod, validators, coerces) as ZKit;
+
+Object.defineProperty(z, "locale", {
+  value: zodLocale,
+  writable: false,
+  enumerable: true,
+});
+
+declare namespace z {
+  export type infer<T extends zod.ZodTypeAny> = zod.infer<T>;
+  export type input<T extends zod.ZodTypeAny> = zod.input<T>;
+  export type output<T extends zod.ZodTypeAny> = zod.output<T>;
+}
 
 export { z };
-export type * as ZTypes from "zod";
+export type * from "zod";
